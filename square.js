71f3859a38c4ddd23a5ccd5a9b8c8bbf03f34622
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-04 13:34:54 -0500

    Update square.js

--- square.js
+++ square.js
@@ -11,4 +11,14 @@ function Square(x,y,w,h,col){
     rect(this.x, this.y, this.w, this.h)
     pop()
   }
+  this.isClicked = function(){
+    if(mouseX > this.x &&
+      mouseX < this.x + this.w &&
+      mouseY > this.y && 
+      mouseY < this.y + this.h &&
+      mouseIsPressed){
+      this.x = Math.floor(Math.random()*390)
+      this.y = Math.floor(Math.random()*390)
+    }
+  }
 }
