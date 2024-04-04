baa8802d0d17b3cc906f74ac082e7f60aca56a0c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-04 13:09:18 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,27 +1,32 @@
 let myFont;
 let s;
-let rnd;
+let rndX;
+let rndY;
 function setup() {
   createCanvas(400, 400);
-  myFont = loadFont("Anta.ttf")
-  rnd  = Math.random();
-  console.log(rnd)
-  s = new Square(190,190,20,20,"#00B4D8")
+  myFont = loadFont("Anta.ttf");
+  generateRndNum();
+  s = new Square(rndX, rndY, 20, 20, "#00B4D8");
 }
-
 function draw() {
   background("#0077B6");
-  textStuff()
-  squareStuff()
-}
-function squareStuff(){
-  s.display();
-}
-function textStuff(){
-  push()
-  fill("#CAF0F8")
-  textSize(20)
-  textFont(myFont)
-  text("Clicker Game" , 125, 30)
+  textStuff();
+  squareStuff();
 }
 
+function generateRndNum() {
+  rndX = Math.random() * 400;
+  rndX = Math.floor(rndX);
+  rndY = Math.random() * 400;
+  rndY = Math.floor(rndY);
+}
+function squareStuff() {
+  s.display();
+}
+function textStuff() {
+  push();
+  fill("#CAF0F8");
+  textSize(20);
+  textFont(myFont);
+  text("Clicker Game", 125, 30);
+}
