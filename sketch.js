f686b2e1478b2c00bca65e4d8245d49341c0ab3c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-04-04 12:49:18 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,24 @@
+let myFont;
+let s;
+function setup() {
+  createCanvas(400, 400);
+  myFont = loadFont("Anta.ttf")
+  s = new Square(190,190,20,20,"#00B4D8")
+}
+
+function draw() {
+  background("#0077B6");
+  textStuff()
+  squareStuff()
+}
+function squareStuff(){
+  s.display();
+}
+function textStuff(){
+  push()
+  fill("#CAF0F8")
+  textSize(20)
+  textFont(myFont)
+  text("Clicker Game" , 125, 30)
+}
+
